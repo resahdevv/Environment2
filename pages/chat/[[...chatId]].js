cc20df8f85a49c5c927a958ea2854c88e4e5bd78
Reza Yna <rezaheryana76@gmail.com>
2023-07-18 15:20:32 +0700

    yo

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -5,27 +5,27 @@
  * Don't delete this message
  */
 
-import { getSession } from "@auth0/nextjs-auth0";
-import { faRobot } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ChatSidebar } from "components/ChatSidebar";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { streamReader } from "openai-edge-stream";
+import { v4 as uuid } from "uuid";
 import { Message } from "components/Message";
+import { useRouter } from "next/router";
+import { getSession } from "@auth0/nextjs-auth0";
 import clientPromise from "lib/mongodb";
 import { ObjectId } from "mongodb";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { streamReader } from "openai-edge-stream";
-import { useEffect, useState } from "react";
-import { v4 as uuid } from "uuid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
 
-export default function ChatPage({ chatId, title, messages = [] }) {
+export default function ChatPage({chatId, title, messages = []}) {
   // console.log("props: ", title, messages);
   const [newChatId, setNewChatId] = useState(null);
   const [incomingMessage, setIncomingMessage] = useState("");
   const [messageText, setMessageText] = useState("");
   const [newChatMessages, setNewChatMessages] = useState([]);
   const [generatingResponse, setGeneratingResponse] = useState(false);
-  const [fullMessage, setFullMessage] = useState("");
+  const [fullMessage, setFullMessage] =useState("");
   const [originalChatId, setOriginalChatId] = useState(chatId);
   const router = useRouter();
 
@@ -37,22 +37,19 @@ export default function ChatPage({ chatId, title, messages = [] }) {
     setNewChatId(null);
   }, [chatId]);
 
-  // save the newly streamed message to new chat messages
+  // save newly streamed message to new chat messages
   useEffect(() => {
     if (!routeHasChanged && !generatingResponse && fullMessage) {
-      setNewChatMessages((prev) => [
-        ...prev,
-        {
-          _id: uuid(),
-          role: "assistant",
-          content: fullMessage,
-        },
-      ]);
-      setFullMessage("");
+      setNewChatMessages(prev => [...prev, {
+        _id: uuid(),
+        role: "assistant",
+        content: fullMessage
+      }])
+      setFullMessage();
     }
   }, [generatingResponse, fullMessage, routeHasChanged]);
 
-  // if we've created a new chat
+  // if we've created new chat
   useEffect(() => {
     if (!generatingResponse && newChatId) {
       setNewChatId(null);
@@ -64,7 +61,7 @@ export default function ChatPage({ chatId, title, messages = [] }) {
     e.preventDefault();
     setGeneratingResponse(true);
     setOriginalChatId(chatId);
-    setNewChatMessages((prev) => {
+    setNewChatMessages(prev => {
       const newChatMessages = [
         ...prev,
         {
@@ -77,13 +74,12 @@ export default function ChatPage({ chatId, title, messages = [] }) {
     });
     setMessageText("");
 
-    //console.log("NEW CHAT: ", json);
     const response = await fetch(`/api/chat/sendMessage`, {
       method: "POST",
       headers: {
-        "content-type": "application/json",
+        'content-type': 'application/json'
       },
-      body: JSON.stringify({ chatId, message: messageText }),
+      body: JSON.stringify({chatId, message: messageText})
     });
     const data = response.body;
     if (!data) {
@@ -97,7 +93,7 @@ export default function ChatPage({ chatId, title, messages = [] }) {
       if (message.event === "newChatId") {
         setNewChatId(message.content);
       } else {
-        setIncomingMessage((s) => `${s}${message.content}`);
+        setIncomingMessage(s => `${s}${message.content}`);
         content = content + message.content;
       }
     });
@@ -107,63 +103,54 @@ export default function ChatPage({ chatId, title, messages = [] }) {
     setGeneratingResponse(false);
   };
 
-  const allMessages = [...messages, ...newChatMessages];
+  const allMesage = [...messages, ...newChatMessages];
 
   return (
     <>
       <Head>
-        <title>New chat</title>
+        <title>New Chat</title>
       </Head>
       <div className="grid h-screen grid-cols-[260px_1fr]">
         <ChatSidebar chatId={chatId} />
         <div className="flex flex-col overflow-hidden bg-gray-700">
-          <div className="flex flex-1 flex-col-reverse overflow-y-auto text-white">
-            {!allMessages.length && !incomingMessage && (
-              <div className="m-auto flex items-center justify-center text-center">
+          <div className="flex-1 flex flex-col-reverse overflow-y-auto text-white">
+            {!allMesage.length && !incomingMessage && (
+              <div className="m-auto justify-center flex items-center text-center">
                 <div>
-                  <FontAwesomeIcon
-                    icon={faRobot}
-                    className="text-6xl text-emerald-200"
-                  />
-                  <h1 className="mt-2 text-4xl font-bold text-white/50">
-                    Ask me a question!
-                  </h1>
+                <FontAwesomeIcon icon={faRobot} className="text-6xl text-emerald-200" />
+                <h1 className=" mt-2 text-4xl font-bold text-white/50">Ask me a question!</h1>
                 </div>
-              </div>
+                </div>
             )}
-            {!!allMessages.length && (
-              <div className="mb-auto">
-                {allMessages.map((message) => (
-                  <Message
-                    key={message._id}
-                    role={message.role}
-                    content={message.content}
-                  />
-                ))}
-                {!!incomingMessage && !routeHasChanged && (
-                  <Message role="assistant" content={incomingMessage} />
-                )}
-                {!!incomingMessage && !!routeHasChanged && (
-                  <Message
-                    role="notice"
-                    content="Only one message at a time. Please allow any other responses to complete before sending another message"
-                  />
-                )}
-              </div>
+            {!!allMesage.length && (
+            <div className="mb-auto">
+            {allMesage.map((message) => (
+              <Message
+              key={message._id}
+              role={message.role}
+              content={message.content}
+              />
+            ))}
+            {!!incomingMessage && !routeHasChanged && (
+            <Message role="assistant" content={incomingMessage} />
             )}
-          </div>
+            {!!incomingMessage && !!routeHasChanged && (
+            <Message
+            role="notice"
+            content="Only one message at a time. Please allow any other responses to complete before sending another message" />
+            )}
+            </div>
+            )}
+            </div>
           <footer className="bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
               <fieldset className="flex gap-2" disabled={generatingResponse}>
                 <textarea
-                  value={messageText}
-                  onChange={(e) => setMessageText(e.target.value)}
-                  placeholder={generatingResponse ? "" : "Send a message..."}
-                  className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
-                />
-                <button type="submit" className="btn">
-                  Send
-                </button>
+                value={messageText}
+                onChange={e => setMessageText(e.target.value)}
+                placeholder={generatingResponse ? "" : "Send a message..."}
+                className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500" />
+                <button type="submit" className="btn">Send</button>
               </fieldset>
             </form>
           </footer>
@@ -188,7 +175,7 @@ export const getServerSideProps = async (ctx) => {
       };
     }
 
-    const { user } = await getSession(ctx.req, ctx.res);
+    const {user} = await getSession(ctx.req, ctx.res);
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_NAME);
     const chat = await db.collection(process.env.MONGODB_COLLECTION).findOne({
@@ -208,7 +195,7 @@ export const getServerSideProps = async (ctx) => {
       props: {
         chatId,
         title: chat.title,
-        messages: chat.messages.map((message) => ({
+        messages: chat.messages.map(message => ({
           ...message,
           _id: uuid(),
         })),
@@ -217,5 +204,5 @@ export const getServerSideProps = async (ctx) => {
   }
   return {
     props: {},
-  };
+  }
 };
